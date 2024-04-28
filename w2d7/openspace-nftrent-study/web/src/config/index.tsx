e6d4151b45b351a8054aa977f40ed5c9d190e859
Noah <upupqi.cs@gmail.com>
2024-04-29 01:39:28 +0800

    chore: change some files

--- w2d7/openspace-nftrent-study/web/src/config/index.tsx
+++ w2d7/openspace-nftrent-study/web/src/config/index.tsx
@@ -1,7 +1,7 @@
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 
 import { cookieStorage, createStorage } from "wagmi";
-import { mainnet, sepolia } from "wagmi/chains";
+import { mainnet, sepolia, optimism } from "wagmi/chains";
 
 // Get projectId at https://cloud.walletconnect.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
@@ -16,7 +16,7 @@ const metadata = {
 };
 
 // Create wagmiConfig
-const chains = [mainnet, sepolia] as const;
+const chains = [mainnet, sepolia, optimism] as const;
 export const config = defaultWagmiConfig({
     chains,
     projectId,
