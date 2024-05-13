da2bdcd8707c4804271b9ce8aea197920c5b86a1
upupnoah <upupqi.cs@gmail.com>
2024-05-13 19:03:19 +0800

    Initialize subgraph

---
+++ w3d2-thegraph/noah-nftmarket/src/noah-nft-market.ts
@@ -0,0 +1,52 @@
+import {
+  NFTCreated as NFTCreatedEvent,
+  NFTRegesitered as NFTRegesiteredEvent,
+  OwnershipTransferred as OwnershipTransferredEvent
+} from "../generated/noah-nft-market/noah-nft-market"
+import {
+  NFTCreated,
+  NFTRegesitered,
+  OwnershipTransferred
+} from "../generated/schema"
+
+export function handleNFTCreated(event: NFTCreatedEvent): void {
+  let entity = new NFTCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.nftCA = event.params.nftCA
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleNFTRegesitered(event: NFTRegesiteredEvent): void {
+  let entity = new NFTRegesitered(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.nftCA = event.params.nftCA
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
